ad91291c33e2e6a31f438f676d984d134793b14c
unknown <db47503@WD166460.uk.ssegroup.net>
2016-05-17 10:15:35 +0100

    Initial Hello World for .Net Tutorial

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+namespace Sse.EnergySystems.Sandbox
+{
+  using System;
+  
+  public class HelloWorld
+  {
+    static void Main(string[] args)
+    {
+      Console.Out.WriteLine("test");
+    }
+  }
+}
